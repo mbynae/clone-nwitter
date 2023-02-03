8e0783a43d9ec4b95ff9193fac4a8ac5c4af65bc
mbynae <piowm123@gmail.com>
2023-02-03 18:25:14 +0900

    nwweter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-    return <span>Home</span>;
+    const [inputs, setInputs] = useState('');
+
+    const onSubmit = e => {
+        e.preventDefault();
+    };
+
+    const onChange = e => {
+        const { value } = e.target;
+        setInputs(value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={inputs} placeholder="작성해주세요" maxLength={120} onChange={onChange} />
+                <input type="submit" value="트윗" />
+            </form>
+        </div>
+    );
 };
 
 export default Home;
